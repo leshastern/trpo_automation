54f3760fb89fec6b5d262b3ea71b249283eb8af3
Андрей Ельцов <andreq64AhA@gmail.com>
2020-03-01 15:37:52 +0300

    Почистил ненужное

--- main.py
+++ main.py
@@ -2,63 +2,54 @@ import re
 import json
 
 
-def validation_subject(subject):
+def validation_subject(subject):  # Проверяю шаблон ТРПО, ЛР и номер лабы
     subject = subject.lower()
-    if subject[0:4] != "трпо":
-        return "subject_exception"
+    if subject[0:4] != "трпо":  # Проверка на абр. ТРПО
+        return False
     index = subject.find("лр", 0, 10)
+    if index == -1:  # Проверка абр. ЛР
+        return False
     number_work = subject[index + 2:index + 5]
-    if number_work[0].isdigit() and number_work[1].isdigit():
-        return number_work[0:2]
-    elif number_work[0].isdigit():
-        return number_work[0]
+    if (number_work[0].isdigit() and number_work[1].isdigit()) or number_work[0].isdigit():
+        return True
     else:
         number_work = number_work[1:3]
-    if number_work[0].isdigit() and number_work[1].isdigit():
-        return number_work
-    elif number_work[1].isdigit():
-        return number_work[1]
+    if (number_work[0].isdigit() and number_work[1].isdigit()) or number_work[1].isdigit():
+        return True
     else:
-        return "subject_exception"
+        return False
 
 
-def validation_body(body):
+def validation_body(body):  # Проверяю приветствие, ссылки и подпись
+    salutation = ["здравствуйте", "добрый", "привет"]  # Шаблоны приветствия
     links = []
     strings = body.split('\n')
-    for item in strings:
+    for item in strings:  # Убираю все лишнее
         if item == "" or item == "--":
             strings.remove(item)
     if len(strings) < 3:
-        return "body_exception"
-    name = strings[len(strings) - 1]
-    name = re.split(' ', name)
-    name[1] = name[1].replace(',', '')  # Удалил запятую
-    FirstName = name[0]
-    LastName = name[1]
-    Group = name[2]
-    for item in strings:
-        pattern = re.findall(r'https://[^ ]*', item)  # [^ ]*
+        return False
+    hello = re.match(r'\w+', strings[0])  # Проверка приветствия по шаблону
+    hello = hello.group(0)
+    if hello in salutation is None:
+        return False
+    name = strings[len(strings) - 1]  # Это подпись
+    res = re.match(r'\w+[ ]?\w+[, ]{2}\d{2}[-]?\w{4}[-]?\d\w', name)  # Проверяю подпись
+    if res is None:
+        return False
+    for item in strings:  # Проверяю ссылки
+        pattern = re.findall(r'https://[^ ]*', item)
         if len(pattern) != 0:
             links.append(pattern)
-    if len(links) == 0:
-        return "links_exception"
-    return FirstName, LastName, Group, links
+    if len(links) == 0:  # Если ссылок нет, то False
+        return False
+    return True
 
 
 def validation(subject, body):
-    try:
-        NumberWork = validation_subject(subject)
-        FirstName, LastName, Group, Links = validation_body(body)
-        data = [FirstName, LastName, Group, NumberWork, Links]
-        filename = "student_information.txt"
-        file = open(filename, mode='w', encoding='utf-8')
-        json.dump(data, file, ensure_ascii=False)
-        file.close()
-    except ChildProcessError:
-        if NumberWork.isdigit():
-            return "validation_body_error"
-        else:
-            return "validation_subject_error"
+    if validation_subject(subject) is True and validation_body(body) is True:
+        return True
+    return False
 
 
 ex0 = "ТРПО ЛР08 название"
@@ -71,4 +62,45 @@ https://www.draw.io/#G1u2wTwjbTJHQQe1qTiWlYytpz_a8huL9I  https://tproger.ru/tran
 Андрей Ельцов, 18-ИСбо-2б"""
 
 
-validation(ex0, ex1)
+print(validation(ex0, ex1))
+
+
+# subject = subject.lower()
+#     if subject[0:4] != "трпо":  # Проверка на абр. ТРПО
+#         return false
+#     index = subject.find("лр", 0, 10)  # Поиск абр. ЛР
+#     number_work = subject[index + 2:index + 5]
+#     if number_work[0].isdigit() and number_work[1].isdigit():
+#         return number_work[0:2]
+#     elif number_work[0].isdigit():
+#         return number_work[0]
+#     else:
+#         number_work = number_work[1:3]
+#     if number_work[0].isdigit() and number_work[1].isdigit():
+#         return number_work
+#     elif number_work[1].isdigit():
+#         return number_work[1]
+#     else:
+#         return false
+
+
+# links = []
+#     strings = body.split('\n')
+#     for item in strings:
+#         if item == "" or item == "--":
+#             strings.remove(item)
+#     if len(strings) < 3:
+#         return "body_exception"
+#     name = strings[len(strings) - 1]
+#     name = re.split(' ', name)
+#     name[1] = name[1].replace(',', '')  # Удалил запятую
+#     FirstName = name[0]
+#     LastName = name[1]
+#     Group = name[2]
+#     for item in strings:
+#         pattern = re.findall(r'https://[^ ]*', item)  # [^ ]*
+#         if len(pattern) != 0:
+#             links.append(pattern)
+#     if len(links) == 0:
+#         return "links_exception"
+#     return FirstName, LastName, Group, links
