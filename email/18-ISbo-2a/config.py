44e08605c250123de6693f1cf7a80c53b55ce807
svyat935 <svyat935@mail.ru>
2020-03-02 20:15:08 +0300

    Добавлена комментарии к функци. Конфиги размещены в отдельном файле config.py. Пустой файл Example.json для примера расположения.

---
+++ email/18-ISbo-2a/config.py
@@ -0,0 +1,9 @@
+"""
+Ссылка на google.drive c инструкцией
+>>> https://docs.google.com/document/d/1rARH0Hdzq7w9ygDbpsrkZ9Pnw82p6WSHQ5AeUT8jOzA/edit
+"""
+
+#id таблицы
+SPREAD_SHEET_ID = "ExampleIdSheet"
+#Ключ Google API
+CREDENTIALS_FILE = "Example.json"
--- email/18-ISbo-2a/excel_add_mark.py
+++ email/18-ISbo-2a/excel_add_mark.py
@@ -1,11 +1,17 @@
 # coding: utf-8
 import logging
 import logging.config
+from config import SPREAD_SHEET_ID
+from config import CREDENTIALS_FILE
 
 logging.config.fileConfig('logging_config.conf')
 logger = logging.getLogger(__name__)
 
-def AddMarkInTable(table, cell, mark):
+def add_mark_in_table(table, cell, mark):
+    """
+    Добавление отметки в журнал. 
+    table - название таблицы. cell - ячейка в таблице. mark - отметка для ячейки.
+    """
     import httplib2 
     import apiclient.discovery
     from oauth2client.service_account import ServiceAccountCredentials	
@@ -13,22 +19,16 @@ def AddMarkInTable(table, cell, mark):
     logger.info('Got into the AddMarkInTable method')
     logger.debug(f'table - {table}, cell - {cell}, mark - {mark}')
     try:
-        #Ключ API для работы
-        CREDENTIALS_FILE = 'GoogleApiKey.json' 
-
         credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
 
         httpAuth = credentials.authorize(httplib2.Http())
         service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) 
-
-        #Id таблицы
-        spreadsheetId = '1Hq6BrKSUtFnIV0_rdCaiH36s2NwdIbobTImhJnbeGOE'
           
         rangeTab = str(table) + "!" + str(cell)
 
         #Сам метод добавления
         #Если пиздец! То лазить тут!
-        service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
+        service.spreadsheets().values().batchUpdate(spreadsheetId = SPREAD_SHEET_ID, body = {
             "valueInputOption": "USER_ENTERED",
             "data": [
                 {"range": rangeTab,
