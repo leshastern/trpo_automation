c5ee39f59790441439aaaada69dfd4cd6330a073
Majishpro <majishpro@gmail.com>
2020-03-03 22:52:45 +0300

    Добавил немного ООП

--- tablica1.py
+++ tablica1.py
@@ -4,17 +4,20 @@ import httplib2
 import apiclient.discovery
 import re;
 from oauth2client.service_account import ServiceAccountCredentials
-CREDENTIALS_FILE = 'наш json файл'  #  ← имя скаченного файла с закрытым ключом
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',                                                                               'https://www.googleapis.com/auth/drive'])
-httpAuth = credentials.authorize(httplib2.Http())
-service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-spreadsheetId = 'ссылка на нашу таблицу'
-range_name = 'Лист1!B1:B1000'
-table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute()
-print(table)
-a='почта пользователя'
-result=re.search(a, str(table))
-if result != None:
-  print('Почта найдена')
-else:
- print('Почты нет в списке')
+class Tablica:
+ def my_metod(a):
+  CREDENTIALS_FILE = 'json файл'  #  ← имя скаченного файла с закрытым ключом
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',                                                                               'https://www.googleapis.com/auth/drive'])
+  httpAuth = credentials.authorize(httplib2.Http())
+  service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+  spreadsheetId = 'наша ссылка на таблицу'
+  range_name = 'Лист1!B3:B1000'
+  table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute()
+  result=re.search(a, str(table))
+  if result != None:
+   b="Почта найдена"
+  else:
+   b="Почты нет в списке"
+  return b
+
+
