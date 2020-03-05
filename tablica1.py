1b573929197864cfa9fefc3c8435d3b13397f97e
Majishpro <majishpro@gmail.com>
2020-03-05 20:42:52 +0300

    Добавил метод очистки email

--- tablica1.py
+++ tablica1.py
@@ -4,20 +4,27 @@ import httplib2
 import apiclient.discovery
 import re;
 from oauth2client.service_account import ServiceAccountCredentials
-class Tablica:
- def my_metod(a):
+#email="Name Surname <1234@gmail.com>" ← пример email , который передает Саша
+def cleaning_email(email):
+ comp = re.compile(r'<(\S*?)>')
+ y=comp.search(email)
+ q=y.group(0)
+ z=q.replace('<','').replace('>','')
+ return z
+def Search_email(email):
+  email=cleaning_email(email)
   CREDENTIALS_FILE = 'json файл'  #  ← имя скаченного файла с закрытым ключом
   credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',                                                                               'https://www.googleapis.com/auth/drive'])
   httpAuth = credentials.authorize(httplib2.Http())
   service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-  spreadsheetId = 'наша ссылка на таблицу'
-  range_name = 'Лист1!B3:B1000'
+  spreadsheetId = 'ссылка на таблицу'
+  range_name = 'Лист1!B1:B1000'
   table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute()
-  result=re.search(a, str(table))
+  print(table)
+  result=re.search(email, str(table))
   if result != None:
-   b="Почта найдена"
+   b=1
   else:
-   b="Почты нет в списке"
+   b=0
   return b
-
 
