758820424a26b823fdbf1b63e659b7da1f150757
Alexander Smirnov <54988303+0Meridian0@users.noreply.github.com>
2020-03-05 21:48:24 +0300

    Add files via upload
    Добавлен поиск информации тела и темы письма, обновлены разрешающие области

--- email_api.py
+++ email_api.py
@@ -11,11 +11,12 @@ import logging.config
 
 logging.config.fileConfig('logging_config.conf')
 logger = logging.getLogger(__name__)
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+service = get_service()
+user_id = 'me'
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.labels']
 
 
-def get_message(service, user_id, msg_id):
+def get_message(service, user_id):
 	logger.info('Got into the get_message method')
 	try:
 		search_id = service.users().messages().list(userId=user_id, labelIds = ['INBOX']).execute()
@@ -26,28 +27,42 @@ def get_message(service, user_id, msg_id):
 		number_results = search_id['resultSizeEstimate']
 		logger.debug(f'number_results - {number_results}')
 
-		iter=0
-		email_stack = []
 		while search_id['resultSizeEstimate'] > 0:
 			alone_msg = message_id[0]
 			id_of_msg = alone_msg['id']
-			message_list = service.users().messages().get(userId=user_id, id=id_of_msg, format='raw').execute()
-			msg_raw = base64.urlsafe_b64decode(message_list['raw'].encode('ASCII'))
-			msg_str = email.message_from_bytes(msg_raw)
-			email_id = msg_str.get_all('From')
-			email_stack[iter] = email_id
+			message_list = service.users().messages().get(userId=user_id, id=id_of_msg, format='full').execute()
+			
+			#Часть сообщения верхнего уровня с полезной информацией
+			info_of_msg = message_list.get('payload')['headers']
 
-			#уточнить дальнейшие действия с найденным майлом
 
-			if #метод Никиты отработал и нашел человека
+			email_id = '' # Имя и ГуглМаил отправителя
+			head_of_msg = '' # Тема письма
+			body_of_msg = '' # Тело письма
+
+
+			for head in info_of_msg :
+				if head['name'] == 'From' :
+					email_id = head['value']
+				if head['name'] == 'Subject' :
+					head_of_msg = head['value']
+			body_of_msg = message_list['snippet']
+
+
+			#if #метод Никиты отработал и нашел человека
 				#отправляем письмо с фразами, что работа принята к рассмотрению
-				#включаются остальные методы
-			else #метод не нашел человека
-				#включаем метод Макса(валидация)
+			#	if #метод Макса отработал и студент правильно запомнил 
+					#сообщение
+					##включаются остальные методы проверки работы
+			#	else #если сообщение заполнено неверно, то высылается 
+					#письмо отправителю о правильном заполнении писем
+
+			#else #метод не нашел человека
+				#отправляется письмо на авторизацию пользователя
 
 
 
-			#архивация сообщения(Разобраться)
+			#архивация сообщения(403 недостаточно прав для gmail api с python)
 			label_id = 'id_of_msg' 
 			# ID of user label to add
 			filter = {
@@ -59,7 +74,7 @@ def get_message(service, user_id, msg_id):
 					'removeLabelIds': ['INBOX']
 				}
 				}
-			result = gmail_service.users().settings().filters().create(userId=user_id, body=filter).execute()
+			result = service.users().settings().filters().create(userId=user_id, body=filter).execute()
 			logger.info('Created filter: %s' % result.get('id'))			
 
 
@@ -77,59 +92,28 @@ def get_message(service, user_id, msg_id):
 		return messages
 
 
-
-def search_messages(service, user_id, search_string):
-	logger.info('Got into the search_messages method')
-	try:
-		search_id = service.users().messages().list(userId=user_id, q=search_string).execute()
-		logger.debug(f'search_id - {search_id}')
-		message_id = search_id['messages']
-		logger.debug(f'message_id - {message_id}')
-
-		number_results = search_id['resultSizeEstimate']
-		logger.debug(f'number_results - {number_results}')
-		final_list = []
-		if number_results>0:
-			massages_ids = search_id['messages']
-			logger.debug(f'massages_ids - {massages_ids}')
-			for ids in massages_ids:
-				final_list.append(ids['id'])
-			logger.debug(f'final_list - {final_list}')
-			return final_list
-		else:
-			print('There were 0 results fot that string, returing an empty string')
-			return ""
-
-	except (errors.HttpError. error):
-		logger.exception(("An error occured: %s") % error)
-	except Exception as ex:
-		logger.exception(ex)
-	finally:
-		logger.info('The search_messages method has completed its execution')
-
-
 def get_service():
 	logger.info('Got into the get_service method')
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+	creds = None
+	 # The file token.pickle stores the user's access and refresh tokens, and is
+	 # created automatically when the authorization flow completes for the first
+	 # time.
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
 
-    service = build('gmail', 'v1', credentials=creds)
+	service = build('gmail', 'v1', credentials=creds)
+	print(service)
 	logger.debug(f'service - {service}')
 	logger.info('The get_service method has completed its execution')
-    return service
+	return service
