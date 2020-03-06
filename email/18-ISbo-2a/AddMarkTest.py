958b1bedef86f43e094e358b83fa14d635ef7bf0
Yuri Pleshkov <55210615+MoL4okkk@users.noreply.github.com>
2020-03-06 09:05:41 +0300

    Исправление тестов

--- email/18-ISbo-2a/AddMarkTest.py
+++ email/18-ISbo-2a/AddMarkTest.py
@@ -10,15 +10,15 @@ class AddMarkTest(unittest.TestCase):
 
     def test_add_mark(self):
 
-        add_mark_in_table('18-ИСбо-2а', D10, 1)
+        add_mark_in_table('18-ISbo-2a', D10, 1)
 
         credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
         httpAuth = credentials.authorize(httplib2.Http())
         service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
-        range = '18-ИСбо-2а!D10'
+        range = '18-ISbo-2a!D10'
 
-        request = service.spreadsheets().values().get(spreadsheetId = SPREAD_SHEET_ID, range)
+        request = service.spreadsheets().values().get(spreadsheetId = SPREAD_SHEET_ID, ranges = range)
         
         self.assertEqual(reques.values(), 1)
 
