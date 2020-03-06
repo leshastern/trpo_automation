4018d5376789eb6e66de3004e0565af4a9ca15d2
Majishpro <majishpro@gmail.com>
2020-03-06 17:11:34 +0300

    Добавил комментари

--- tablica1.py
+++ tablica1.py
@@ -6,22 +6,32 @@ import re;
 from oauth2client.service_account import ServiceAccountCredentials
 #email="Name Surname <1234@gmail.com>" ← пример email , который передает Саша
 def cleaning_email(email):
+ """
+ Метод для выделения почты из передаваемой строки email.
+ email - передаваемая строка с почтой
+ Name Surname <1234@gmail.com> ← пример email который мне передают
+ 1234@gmail.com это будет запоминаться после метода очистки
+ """
  comp = re.compile(r'<(\S*?)>')
  y=comp.search(email)
  q=y.group(0)
  z=q.replace('<','').replace('>','')
  return z
 def Search_email(email):
-  email=cleaning_email(email)
+  """
+   Метод для поиска в таблице.
+   email - передаваемая строка с почтой
+  """
+  email=cleaning_email(email) # вызываю метод очистки строки в нужный формат
   CREDENTIALS_FILE = 'json файл'  #  ← имя скаченного файла с закрытым ключом
   credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',                                                                               'https://www.googleapis.com/auth/drive'])
   httpAuth = credentials.authorize(httplib2.Http())
   service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
   spreadsheetId = 'ссылка на таблицу'
   range_name = 'Лист1!B1:B1000'
-  table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute()
-  print(table)
-  result=re.search(email, str(table))
+  table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute() 
+  # table- выводит нам словарь всех значение в ячейках B1:B1000
+  result=re.search(email, str(table)) # поиск почты 
   if result != None:
    b=1
   else:
