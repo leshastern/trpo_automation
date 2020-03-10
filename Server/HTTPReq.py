fef2be4c5a5b59f61585b4626790735a14cef082
Progoger <rastorguev10@gmail.com>
2020-03-10 08:37:15 +0300

    ChangedSys
    Заменил один файл на два, один http-запрос, другой отправка JSON

--- Server/HTTPReq.py
+++ Server/HTTPReq.py
@@ -2,19 +2,5 @@ import requests
 
 r = requests.get('http://127.0.0.1:8080/')
 print(r.text)
-contacts_data = {
-  "givenName": "Pavel",
-  "surname": "Bansky",
-  "emailAddresses": [
-    {
-      "address": "pavelb@fabrikam.onmicrosoft.com",
-      "name": "Pavel Bansky"
-    }
-  ],
-  "businessPhones": [
-    "+1 732 555 0102"
-  ]
-}
-r = requests.post('http://127.0.0.1:8080/', json=contacts_data)
-print(r)
+
 
