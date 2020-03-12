c137d064f978ceab120972020daf45cef4634bfb
ShapovalovQ <amr15319@gmail.com>
2020-03-12 19:04:05 +0300

    Версия 2.0

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -3,26 +3,31 @@ import socket
 import json
 import random
 
-class Server:
 
-    #def server_otvet():
-    
-        sock = socket.socket()
-        sock.bind(('', 12222))
-        sock.listen(1)
-        conn, addr = sock.accept()
-        while True:
-             data = conn.recv(1024)
-             if not data:
-                   break
-             r = random.randint(0, 1)
-             data_j={
-             "labStatus": r,
-                      }
-             y = json.dumps(data_j)
-             conn.send(y.encode())
 
-        conn.close()      
+def server_otvet(port):
+   """
+   Метод "заглушки" для сервера
+   port - по какому порту будет настроено соединение
+   (в дальнейшем, можно будет узнать номер лабораторной по номеру порта)
+   """
+
+   sock = socket.socket()
+   sock.bind(('127.0.0.1', port))
+   sock.listen(1)
+   conn, addr = sock.accept()
+   while True:
+      data = conn.recv(1024)
+      if not data:
+         break
+      r = random.randint(0, 1)
+      data_j={
+      "labStatus": r,
+      }
+      y = json.dumps(data_j)
+      conn.send(y.encode())
+
+   conn.close()     
         
             
 
