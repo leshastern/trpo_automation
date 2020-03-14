d2fbd527a75b1faa8eac9876c2e906f76e98df8c
Progoger <rastorguev10@gmail.com>
2020-03-14 11:00:10 +0300

    FuncWorkWithLetters
    Написал метод WorkWithLetters

--- Server/Server.py
+++ Server/Server.py
@@ -1,21 +1,7 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
 import socket
 import time
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8080))
-serversocket.listen(5)
-print('Server is waiting for connections.')
-while True:
-    conn, addr = serversocket.accept()
-    time.sleep(0.1)
-    data = conn.recv(1024)
-    print('Connection:', addr)
-    print('------------------------------')
-    print("Request Data from Browser")
-    print('------------------------------')
-    print(data)
-    if not data:
-        break
-    conn.send(data)
-    conn.close()
-# Делаем задержку, чтобы цикл не сильно загружал процессор
+server_address = ("127.0.0.1", 8080)
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+httpd.serve_forever()
