7f35ebff48ffafda88b7d7a544b315d6527d5814
VladmirV <vasilje.vova2010@yandex.ru>
2020-03-15 22:42:09 +0300

    Внесение окончательных поправок.

--- Google/Gmail/Gmail.py
+++ Google/Gmail/Gmail.py
@@ -1,10 +1,7 @@
 import smtplib
 from email.message import EmailMessage
-import email
 import config
 import imaplib
-import os
-import base64
 
 
 def smtp_login():
@@ -70,57 +67,33 @@ def send_mess(smtpObj, mailTo, topic, text):
     smtpObj.send_message(mes)
 
 
+def count_unseen_mess(mail):
+    """
+    Возвращает кол-во непрочитанных сообщений
+    :param mail:
+    :return:
+    """
+    result, data = mail.uid('search', None, "unseen")
+    return len(data[0].split())
+
+
 def read(mail):
     """
     Функция считывающая новые gmail сообщения
     :param mail:
     :return:
     """
-    try:
+    count = count_unseen_mess(mail)
+    if count > 0:
+        return_result = []
         result, data = mail.uid('search', None, "unseen")  # Выполняет поиск и возвращает UID писем.
-        latest_email_uid = data[0].split()[-1]
-        result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-        raw_email = data[0][1]
-        try:
-            email_message = email.message_from_string(raw_email)
-        except TypeError:
-            email_message = email.message_from_bytes(raw_email)
-        print (email.header.make_header(email.header.decode_header(email_message['From'])))
-        print (email.header.make_header(email.header.decode_header(email_message['Subject'])))
-        if email_message.is_multipart():
-            for payload in email_message.get_payload():
-                print(payload.get_payload())
-        else:
-            print(email_message.get_payload())
-        if email_message.is_multipart():
-            for part in email_message.walk():
-                filename = part.get_filename()
-                if filename:
-                    with open(part.get_filename(), 'wb') as new_file:
-                        new_file.write(part.get_payload(decode=True))
-                        print("Done")
-    except BaseException:
-        print("Error")
-
-
-#@*-!-+&^* ТЕСТОВЫЕ ЗАПРЕЩЕННЫЕ МАТЕРИАЛЫ #@*-!-+&^*
-#def get_first_text_block(self, email_message_instance):
-#    maintype = email_message_instance.get_content_maintype()
-#    if maintype == 'multipart':
-#        for part in email_message_instance.get_payload():
-#            if part.get_content_maintype() == 'text':
-#                  return part.get_payload()
-#            elif maintype == 'text':
-#              return email_message_instance.get_payload()
-
-
-
-
-#mailTo = "vasilje.vova2010@gmail.com"
-#topic = "Message10"
-#text = "That is works!"
-#smtp = smtp_login()
-#send_mess(smtp, mailTo, topic, text)
-#quit_email(smtp)
-imp = imap_login()
-read(imp)
+        print(count)
+        for i in range(count):
+            latest_email_uid = data[0].split()[i]
+            result, date = mail.uid('fetch', latest_email_uid, '(RFC822)')
+            raw_email = date[0][1]
+            return_result.append(raw_email)
+            print("ready")
+        return return_result
+    else:
+        print("Отсутствие новых сообщений.")
