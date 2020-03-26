b15981935e44735d7f2aa4db31ed2f8e2bd99a54
Yuri Pleshkov <55210615+MoL4okkk@users.noreply.github.com>
2020-03-26 21:29:54 +0300

    Ещё куча тестов (не все работают, правда)

---
+++ email/18-ISbo-2a/Test_client.py
@@ -0,0 +1,49 @@
+import unittest
+import socket
+import threading
+import json
+from config import PORTS
+from client import send_a_laboratory_work_for_verification
+
+
+
+class Test_client(unittest.TestCase):
+
+    def test_1(self):
+
+        #from Test_client import waiting_func
+
+        number = '1'
+        link = 'http://github.com'
+        #server_thread = threading.Thread(target = waiting_func, args=(1, number, link, ), daemon=True)
+        
+        #server_thread.start()
+
+        a = send_a_laboratory_work_for_verification(labNumber = number, labLink = link)
+
+        self.assertEqual(a, 1)
+
+    def test_2(self):
+        
+        number = '1'
+        link = 'http://github.com'
+
+        a = send_a_laboratory_work_for_verification(labNumber = number, labLink = link)
+
+        self.assertIsNone(a)
+
+    def test_3(self):
+
+        #from Test_client import waiting_func
+        
+        number = '1'
+        link = 'http://github.com'
+        #server_thread = threading.Thread(target = waiting_func, args=(0, number, link, ), daemon=True)
+
+        #server_thread.start()
+
+        a = send_a_laboratory_work_for_verification(labNumber = number, labLink = link)
+
+        self.assertEqual(a, 0)
+
+        
---
+++ email/18-ISbo-2a/Test_validation.py
@@ -0,0 +1,106 @@
+import unittest
+
+class Test_validation(unittest.TestCase):
+
+    def test_urlcheack1(self):
+
+        from Validation import url_cheack
+
+        body_of_msg = 'Добрый день! http://github.com.'
+        number = '8'
+
+        URL = url_cheack(number, body_of_msg)
+
+        self.assertEqual(URL, 'http://github.com')
+
+    def test_urlcheack2(self):
+
+        from Validation import url_cheack
+
+        body_of_msg = 'Добрый день! http://github.com.'
+        number = '6'
+
+        URL = url_cheack(number, body_of_msg)
+
+        self.assertIsNone(URL)
+
+    def test_urlcheack3(self):
+
+        from Validation import url_cheack
+
+        body_of_msg = 'Добрый день! http://github.com - ссылка на репозиторий.'
+        number = '8'
+
+        URL = url_cheack(number, body_of_msg)
+
+        self.assertEqual(URL, 'http://github.com')
+
+    def test_urlcheack4(self):
+
+        from Validation import url_cheack
+
+        body_of_msg = 'Добрый день! htp://github.com.'
+        number = '8'
+
+        URL = url_cheack(number, body_of_msg)
+
+        self.assertIsNone(URL)
+    
+    def test_validation1(self):
+
+        from Validation import validation
+
+        head_of_msg = 'ТРПО ЛР№1'
+        body_of_msg = 'Добрый день! Вот ссылка на гитхаб - http://github.com. --подпись типа'       
+        validation_dictionary={ 
+            'Number':'1',
+            'URL': None,
+            'Errors': []
+            }
+
+        answer = validation(head_of_msg, body_of_msg)
+
+        self.assertEqual(answer, validation_dictionary)
+    
+    def test_validation2(self):
+
+        from Validation import validation
+
+        head_of_msg = 'ТИПИС ЛР 1'
+        body_of_msg = 'Салют! Вот ссылка на гитхаб - http://github.com. Кандидат на отчисление'
+        Errors_list = []
+        Errors_list.append('неверно указано название предмета')
+        Errors_list.append('неверно указан номер ЛР')
+        Errors_list.append('отсутсвует приветствие')
+        Errors_list.append('отсутсвует подпись')
+        validation_dictionary={ 
+            'Number':None,
+            'URL': None,
+            'Errors': Errors_list
+            }
+
+        answer = validation(head_of_msg, body_of_msg)
+
+        self.assertEqual(answer, validation_dictionary)
+    
+    def test_validation3(self):
+
+        from Validation import validation
+
+        head_of_msg = 'ТРПО ЛР№8'
+        body_of_msg = 'Салют! Вот ссылка на гитхаб - http://github.com.'
+        Errors_list = []
+        Errors_list.append('отсутсвует приветствие')
+        Errors_list.append('отсутсвует подпись')
+        validation_dictionary={ 
+            'Number':'8',
+            'URL': 'http://github.com',
+            'Errors': Errors_list
+            }
+
+        answer = validation(head_of_msg, body_of_msg)
+
+        self.assertEqual(answer, validation_dictionary)
+
+if __name__ == '__main__':
+    unittest.main()
