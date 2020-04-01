108972efc724154c2a4312be6f0ecbd45294a10b
Aleksey Smirnov <55441212+leshastern@users.noreply.github.com>
2020-04-01 22:24:39 +0300

    Изменение архитектуры

---
+++ LR/07/TCPServer/strategylab.cpp
@@ -0,0 +1,34 @@
+#include "strategylab.h"
+/**
+ * @brief Конструктор
+ * @param objectFromServer
+ * @param parent
+ */
+StrategyLab::StrategyLab(QJsonObject objectFromServer, QObject* parent)
+    : QObject(parent),
+      object(objectFromServer)
+{
+    this->getCode();
+}
+
+/**
+ * @brief Метод для извлечения основного кода программы
+ *  из доставленного Json объекта
+ * @throws QString
+ */
+void StrategyLab::getCode()
+{
+    if (object.contains("code")) {
+        this->code = object.value("code").toString();
+    }
+
+    throw QString("Переданный Json объект не содержит поля 'code'");
+}
+
+/**
+ * @brief Деструктор
+ */
+StrategyLab::~StrategyLab()
+{
+    // TODO дописать
+}
