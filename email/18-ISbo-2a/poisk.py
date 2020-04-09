5f47deb294b773d92394930760e32e3fe07ad60d
Никита Обидин <majishpro@gmail.com>
2020-04-09 21:14:23 +0300

    Обновил

--- email/18-ISbo-2a/poisk.py
+++ email/18-ISbo-2a/poisk.py
@@ -5,19 +5,19 @@ import re;
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 from apiclient import discovery
-email='[Обидин Никита <majishpro@gmail.com>]'
+email='[Surname Name <email@gmail.com>]'
 group='18-ИСбо-2а'
 laba='8'
-surname='Обидин  Никита Викторович'
+surname='ФИО'
 def search_tablic(group,laba, surname):
     group1='(ТРПО) '+group
     a=email
     c=2
-    CREDENTIALS_FILE = 'my-python-268812-20703f3ff8e1.json'
+    CREDENTIALS_FILE = 'json файл'
     credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',  'https://www.googleapis.com/auth/drive'])
     httpAuth = credentials.authorize(httplib2.Http())
     service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
-    spreadsheetId = '1J6hmTxOlfv5iRfG3I6ZIrzjiGDYsbmPZY3JIvJjes4Q'
+    spreadsheetId = 'ссылка на таблицу'
     range_name = group1+'!D2:D1000'
     table = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name).execute()
     count=ord('J')+int(laba)-1
