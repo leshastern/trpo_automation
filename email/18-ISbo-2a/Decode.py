e009921983befcba95fee399416a3aa1c440afa9
Yuri Pleshkov <55210615+MoL4okkk@users.noreply.github.com>
2020-04-10 04:22:21 +0300

    Add files via upload

---
+++ email/18-ISbo-2a/Decode.py
@@ -0,0 +1,19 @@
+import fileinput
+import os
+
+def Decode_files(file_names):
+    for name in file_names:
+        with fileinput.FileInput(name, inplace = True, backup='.bak') as file:
+            for line in file:
+                newline = ''
+                for symbol in line:
+                    symbol = chr(ord(symbol)-1)
+                    newline = newline + symbol
+                print(newline)
+
+def Finish(file_names):
+    for name in file_names:
+        oldname = name
+        newname = name.replace('.bak', '')
+        os.unlink(newname)
+        os.rename(oldname, newname)
