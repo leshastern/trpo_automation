e4b50b1c5665ee5438c4a0674dcee07ccf88b7a0
Yuri Pleshkov <55210615+MoL4okkk@users.noreply.github.com>
2020-04-15 16:38:50 +0300

    Декодер для полей файла config

--- email/18-ISbo-2a/Decode.py
+++ email/18-ISbo-2a/Decode.py
@@ -21,3 +21,24 @@ def Finish(file_names):
         newname = name.replace('.bak', '')
         os.unlink(newname)
         os.rename(oldname, newname)
+
+def Decode_config(config, names):
+    with fileinput.FileInput(config, inplace=True, backup='.bak') as file:
+        for line in file:
+            newline = line
+            for name in names:
+                if line.find(name) != -1:
+                    newline = name + ' = ' + "'"
+                    line = line.replace(newline, '')
+                    line = line.replace("'", '')
+                    for symbol in line:
+                        if symbol == '':
+                            symbol == ''
+                        else:
+                            if symbol != '/n' and symbol != '/t' and symbol != '	':
+                                symbol = chr(ord(symbol)-1)
+                        newline = newline + symbol
+                    newline = newline[:-1]
+                    newline = newline + "'"
+                    names.remove(name)
+            print(newline)
