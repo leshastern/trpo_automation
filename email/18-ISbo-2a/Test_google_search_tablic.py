ab6a0db32f6cc68ca127b8aea1ce40f75605a30f
Maxim <MaXLyuT2000@gmail.com>
2020-04-16 22:06:10 +0300

    Добавил еще больше тестов

--- email/18-ISbo-2a/Test_google_search_tablic.py
+++ email/18-ISbo-2a/Test_google_search_tablic.py
@@ -13,6 +13,28 @@ class Test_google(unittest.TestCase):
         act=search_tablic('18-Исбо-2а','7','Лютый Максим Сергеевич')
         exp='P6'
         self.assertEqual(act, exp)
+    def test_search_tablic1(self):
+        from APIgoogle import search_tablic
+        act=search_tablic('18-Исбо-2б','7','Лютый Максим Сергеевич')
+        exp='P6'
+        self.assertEqual(act, exp)
+    def test_search_tablic2(self):
+        from APIgoogle import search_tablic
+        act=search_tablic('18-Исбо-2б','','Лютый Максим Сергеевич')
+        exp='P6'
+        self.assertEqual(act, exp)
+    def test_search_tablic3(self):
+        from APIgoogle import search_tablic
+        act=search_tablic('18-Исбо-2б','7','Смирнов Александр Алексеевич')
+        exp='P6'
+        self.assertEqual(act, exp)
+    def test_search_tablic3(self):
+        from APIgoogle import search_tablic
+        act=search_tablic('18-Исбо-2б','7','')
+        exp='P6'
+        self.assertEqual(act, exp)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
