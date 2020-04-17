dcec76c9d43e76c771e5ef2c521001868455726d
Progoger <rastorguev10@gmail.com>
2020-04-17 19:55:38 +0300

    Update global_ExOfLetters.py

--- email/18-ISbo-2b/global_ExOfLetters.py
+++ email/18-ISbo-2b/global_ExOfLetters.py
@@ -5,92 +5,92 @@ import global_Letter as Letter
 def create_letters():
     letters = []
     student = User.User("Максим Расторгуев", "18-ИСбо-2", "rastorguev10@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01\n"
-    "С Уважением,\n"
-    "Расторгуев Максим, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01
+С Уважением,
+Расторгуев Максим, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР01", body, 1, 1)
     letters.append(letter)
 
     student = User.User("Валерий Бублин", "18-ИСбо-2", "valerabubla44@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01\n"
-    "С Уважением,\n"
-    "Валерий Бублин, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01
+С Уважением,
+Валерий Бублин, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР02", body, 1, 1)
     letters.append(letter)
 
     student = User.User("Софья Безбрежанова", "18-ИСбо-2", "sofiebezcoast@gmail.com", False)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01\n"
-    "С Уважением,\n"
-    "Софья Безбрежанова, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01
+С Уважением,
+Софья Безбрежанова, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР01", body, 1, 1)
     letters.append(letter)
 
     student = User.User("Григорий Васверидзе", "18-ИСбо-2", "grishaakvasya@gmail.com", False)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01\n"
-    "С Уважением,\n"
-    "Григорий Васверидзе, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01
+С Уважением,
+Григорий Васверидзе, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР01", body, 1, 1)
     letters.append(letter)
 
     student = User.User("Сантьяго Цеместес", "18-ИСбо-2", "santozemesjago@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02\n"
-    "С Уважением,\n"
-    "Сантьяго Цеместес, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02
+С Уважением,
+Сантьяго Цеместес, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР01", body, 1, 1)
     letters.append(letter)
 
     student = User.User("Сантьяго Цеместес", "18-ИСбо-2", "santozemesjago@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02\n"
-    "С Уважением,\n"
-    "Сантьяго Цеместес, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02
+С Уважением,
+Сантьяго Цеместес, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР02", body, 2, 2)
     letters.append(letter)
 
     student = User.User("Борис Кипарис", "18-ИСбо-2", "kiparisforboris@gmail.com", True)
-    body = "Хаюшки, мистер ЮрВикт!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01\n"
-    "С Уважением,\n"
-    "Борис Кипарис, студент группы 18-ИСбо-2б"
+    body = """"Хаюшки, мистер ЮрВикт!
+Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01
+С Уважением,
+Борис Кипарис, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР02", body, 2, 2)
     letters.append(letter)
 
     student = User.User("Сантьяго Цеместес", "18-ИСбо-2", "santozemesjago@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:\n"
-    "А нет, не отправляю\n"
-    "С Уважением,\n"
-    "Сантьяго Цеместес, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:
+А нет, не отправляю
+С Уважением,
+Сантьяго Цеместес, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР02", body, 2, 2)
     letters.append(letter)
 
     student = User.User("Санто Цеметретос", "18-ИСбо-2", "santozement322@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02\n"
-    "С Уважением,\n"
-    "Здесь пригодилась бы подпись"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 2-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР02
+С Уважением,
+Здесь пригодилась бы подпись"""
     letter = Letter.Letter(student, "ТРПО ЛР02", body, 2, 2)
     letters.append(letter)
 
     student = User.User("Максим Расторгуев", "18-ИСбо-2", "rastorguev10@gmail.com", True)
-    body = "Доброго времени суток, Юрий Викторович!\n"
-    "Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:\n"
-    "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2\n"
-    "С Уважением,\n"
-    "Расторгуев Максим, студент группы 18-ИСбо-2б"
+    body = """Доброго времени суток, Юрий Викторович!
+Отправляю вам ссылку на репозиторий с выполненной 1-ой лабораторной:
+https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2
+С Уважением,
+Расторгуев Максим, студент группы 18-ИСбо-2б"""
     letter = Letter.Letter(student, "ТРПО ЛР01", body, 1, 1)
     letters.append(letter)
 
