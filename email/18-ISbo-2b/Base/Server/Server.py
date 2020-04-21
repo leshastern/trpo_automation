5f24a4352f9dfd34525a503351c47d29edfa7e16
Sergey <sepa33126@gmail.com>
2020-04-21 08:13:31 +0300

    Изменение файлов в соответствии с нужной структурой

--- email/18-ISbo-2b/Base/Server/Server.py
+++
@@ -1,7 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import socket
-import time
-
-server_address = ("127.0.0.1", 8080)
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
